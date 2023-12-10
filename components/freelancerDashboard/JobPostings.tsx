0333de00b70156b448472e99d4d40b1153e4c9c2
anonhustler <theanonhustler@gmail.com>
2023-12-10 07:40:23 +0530

    ui fix

--- components/freelancerDashboard/JobPostings.tsx
+++ components/freelancerDashboard/JobPostings.tsx
@@ -20,6 +20,7 @@ function JobPostings() {
       <div className="grid grid-cols-1 gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3">
         {/* @ts-ignore */}
         {postings.filter(
+          // @ts-ignore
           (post) => post.freelancerId === address && !post.completed
         ).length > 0 ? (
           postings?.map((post, idx) => {
